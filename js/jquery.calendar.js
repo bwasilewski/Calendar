a43dc4537e84cc0cf3f2f7efa761ff41dd4971ca
Benjamin Wasilewski <ben.wasilewski@apervita.com>
2016-10-02 15:25:42 -0500

    remove old stylesheets and replace with new sass library. Progress on converting to jquery plugin.

--- js/jquery.calendar.js
+++ js/jquery.calendar.js
@@ -15,6 +15,37 @@
 
         var opts = $.extend( {}, $.fn.calendar.defaults, options );
 
+        $.fn.calendar.options = opts;
+
+        $.fn.calendar.container = $('<div />');
+        $.fn.calendar.titleLabel = $('<p />', {
+            'class': 'calendar-title'
+        });
+        $.fn.calendar.currentLabel = $('<p />', {
+            'class': 'current-label'
+        });
+        $.fn.calendar.displayLabel = $('<p />', {
+            'class': 'display-label'
+        });
+        $.fn.calendar.calendarHeader = $('<thead />', $.fn.calendar.options.calendarHeaderAttributes)
+            .html('<tr><th>S</th><th>M</th><th>T</th><th>W</th><th>T</th><th>F</th><th>S</th></tr>');
+        $.fn.calendar.selectors = $('<form />',
+            {'class': 'selectors' });
+        $.fn.calendar.monthSelector = $('<select />', $.fn.calendar.options.monthSelectorAttributes);
+        $.fn.calendar.yearSelector = $('<select />', $.fn.calendar.options.monthSelectorAttributes);
+        $.fn.calendar.el = $('<table />', $.fn.calendar.options.tableAttributes);
+        $.fn.calendar.row = $('<tr />', $.fn.calendar.options.rowAttributes);
+        $.fn.calendar.rows = [];
+        $.fn.calendar.cell = $('<td />', $.fn.calendar.options.cellAttributes);
+
+        $.fn.calendar.nextButton = $('<button />', {
+            'class': $.fn.calendar.defaults.nextMonthClassName
+        }).text('Next');
+
+        $.fn.calendar.previousButton = $('<button />', {
+            'class': $.fn.calendar.defaults.previousMonthClassName
+        }).text('Previous');
+
         return this.each(function () {
             var container = $.fn.calendar.container;
             var el = $.fn.calendar.el;
@@ -46,14 +77,16 @@
             populateSelectors();
 
             // Event Handlers
-            $(opts.nextMonthSelector).bind('click', function (ev) {
+            $(opts.nextMonthSelector, $.fn.calendar.container).bind('click', function (ev) {
                 var nextMonth = ( month === 11 ) ? 0 : month + 1
                     ,nextYear = ( month === 11 ) ? year + 1 : year;
 
                 $.fn.calendar.updateCalendar(nextMonth, nextYear);
             });
 
-            $(opts.previousMonthSelector).bind('click', function (ev) {
+            console.log('Container: ', $.fn.calendar.container);
+
+            $(opts.previousMonthSelector, $.fn.calendar.container).bind('click', function (ev) {
                 var nextMonth = ( month === 0 ) ? 11 : month - 1
                     ,nextYear = ( month === 0 ) ? year - 1 : year;
 
@@ -65,8 +98,6 @@
                     var m = parseFloat($.fn.calendar.monthSelector.find('option:selected').attr('value'));
                     var y = parseFloat($.fn.calendar.yearSelector.find('option:selected').attr('value'));
 
-                    console.log(m, y);
-
                     $.fn.calendar.updateCalendar(m, y);
                 });
             });
@@ -77,66 +108,55 @@
         'calendarAttrs': {
             'class': 'apervita-calendar'
         },
+        'calendarHeaderAttributes': {
+            'class': 'dayLabels'
+        },
+        'cellAttributes': {
+            'class': 'dateCell'
+        },
         'containerAttrs': {
             'class': 'apervita-calendar-container'
         },
         'message': 'New jQuery Plugin!',
+        'monthSelectorAttributes': {
+            'class': 'month-selector'
+        },
         'nextMonthClassName': 'next',
         'nextMonthSelector': '.next',
         'previousMonthClassName': 'prev',
         'previousMonthSelector': '.prev',
-        'title': 'jQuery Calendar by Ben Wasilewski'
+        'rowAttributes': {
+            'class': 'dateRow'
+        },
+        'tableAttributes': {
+            'class': 'date-table'
+        },
+        'title': 'jQuery Calendar by Ben Wasilewski',
+        'yearSelectorAttributes': {
+            'class': 'year-selector'
+        }
     };
-
-    $.fn.calendar.container = $('<div />');
-    $.fn.calendar.titleLabel = $('<p />', {
-        'class': 'calendar-title'
-    });
-    $.fn.calendar.currentLabel = $('<p />', {
-        'class': 'current-label'
-    });
-    $.fn.calendar.displayLabel = $('<p />', {
-        'class': 'display-label'
-    });
-    $.fn.calendar.selectors = $('<form />',
-        {'class': 'selectors' });
-    $.fn.calendar.monthSelector = $('<select />', {
-        class: 'monthSelector' });
-    $.fn.calendar.yearSelector = $('<select />', {
-        class: 'yearSelector' });
-    $.fn.calendar.el = $('<table />');
-    $.fn.calendar.row = $('<tr />', {
-        'class': 'dateRow' });
-    $.fn.calendar.cell = $('<td />', {
-        'class': 'dateCell' });
-
-    $.fn.calendar.nextButton = $('<button />', {
-        'class': $.fn.calendar.defaults.nextMonthClassName
-    }).text('Next');
-
-    $.fn.calendar.previousButton = $('<button />', {
-        'class': $.fn.calendar.defaults.previousMonthClassName
-    }).text('Previous');
-
 
     function drawCalendar (m, y) {
         var that = this
-            ,calendarRows = [];
+            ,calendarRows = []
+            ,calHeader = $.fn.calendar.calendarHeader;
 
         // TODO: Make this dynamic
-        $('.dateRow').remove();
+        $('.dateRow', $.fn.calendar.el).remove();
+
+        calHeader.appendTo($.fn.calendar.el);
 
         for ( var i = 0; i < 6; i++) {
             var row = $.fn.calendar.row.clone();
 
             row.appendTo($.fn.calendar.el);
-
             calendarRows.push(row);
         }
 
         $.each(calendarRows, function (ind, val) {
             for ( var n = 0; n < 7; n++ ) {
-                var cell = $('<td/>', { 'class': 'dateCell' }).appendTo(val);
+                var cell = $.fn.calendar.cell.clone().appendTo(val);
             }
         });
 
@@ -181,6 +201,7 @@
             });
         }
 
+        // TODO: make this dynamic
         $.each($('.dateCell'), function (ind, val) {
             $(val).addClass(dateArray[ind].className).html(dateArray[ind].date);
         });
@@ -246,7 +267,7 @@
 
     function updateHeaders () {
         $.fn.calendar.currentLabel.html( dayArray[todaysDate.getDay()] + ', ' + monthArray[todaysDate.getMonth()] + ' ' + getOrdinal(todaysDate.getDate()) + ', ' + todaysDate.getFullYear() );
-        $.fn.calendar.displayLabel.html(monthArray[month] + ' ' + year);
+        $.fn.calendar.displayLabel.html( monthArray[month] + ' ' + year );
     };
 
     function getOrdinal (n) {
--- js/main.js
+++ js/main.js
@@ -2,7 +2,16 @@
 
     $('.jquery-calendar-wrap').calendar({
         message: 'Message override!',
-        title: 'jQuery Calendar'
+        title: 'jQuery Calendar',
+        previous: '#apervita-previous',
+        next: '#apervita-next'
+    });
+
+    $('.jquery-calendar-wrap-clone').calendar({
+        message: 'Message override!',
+        title: 'jQuery Calendar Clone',
+        // previous: '#apervita-previous',
+        // next: '#apervita-next'
     });
 
 }(jQuery));
